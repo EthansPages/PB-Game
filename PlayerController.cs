9d4c6ba7c1bc3bc2d8d4ec9950cae9d3ffae21fb
m0kzip <38244251+m0kzip@users.noreply.github.com>
2018-04-10 09:55:32 +0100

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,1 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rigi;
+    public Vector2 MoveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        rigi = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(Input.GetKeyDown(KeyCode.A))
+        {
+            rigi.AddForce(MoveSpeed);
+        }
+
+	}
+}
