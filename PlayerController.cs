6e54b6de704ad53a0269a958e2e3a929ee3b6718
m0kzip <38244251+m0kzip@users.noreply.github.com>
2018-04-10 10:36:53 +0100

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public Rigidbody2D rigi;
-    public Vector2 MoveSpeed;
+    public float MoveSpeed = 3;
+    public Vector2 JumpHeight;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rigi = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		while(Input.GetKeyDown(KeyCode.A))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
         {
-            rigi.AddForce(MoveSpeed);
+            rigi.velocity = new Vector2(MoveSpeed, rigi.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rigi.velocity = new Vector2(-MoveSpeed, rigi.velocity.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigi.AddForce(JumpHeight);
         }
 
-	}
+
+    }
 }
